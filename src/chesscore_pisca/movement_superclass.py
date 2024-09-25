cb2d43894a2a6c37cd60200a4f6a0e59d7e70393
AnRaTheStar17ju1682 <outworlddevourer1337darklordking666@outlook.com>
2024-09-25 18:30:23 +0300

    added 'JumpMovementMode' in the 'movement_superclass' module, it is the way how a knight moves in chess

--- src/chesscore_pisca/movement_superclass.py
+++ src/chesscore_pisca/movement_superclass.py
@@ -20,6 +20,7 @@ class MovementMode:
             
             iteration += 1
             
+            # че если вернется несколько в одной?
             if board.grid[vposition]['piece']:
                 yield vposition
                 break
@@ -49,3 +50,88 @@ class MovementMode:
             new_position.append(new_letter)
 
         return '-'.join(new_position)
+
+
+class JumpMovementMode(MovementMode):
+    path_data = ({'path': '+y +y +x', 'smash': True, 'sectors': 'all'}, 'END')
+    
+    
+    def __init__(self):
+        super().__init__(self)
+    
+    
+    def __call__(self, board, position):
+        def generate_rotated_paths(
+                position,
+                path_data,
+                current_sector='+y+x',
+                relativity=('+y+x', '+y-x', '-x+y', '-x-y', '-y-x', '-y+x', '+x-y', '+x+y')
+            ):
+            # exit condition
+            if path_data[0] == 'END':
+                return []
+            
+            
+            path = path_data[0]['path']
+            smash = path_data[0]['smash']
+            active_sectors = path_data[0]['sectors']
+            smash_list = list()
+            # это нужно для первой итерации
+            old_sector = current_sector
+
+
+            index = relativity.index(current_sector)
+            relativity = relativity[index:] + relativity[:index]
+            
+            
+            for current_sector in relativity:
+                vposition = position
+                
+                
+                # блок обработки пути для нового сектора
+                main = current_sector[:2]
+                sub = current_sector[2:]
+                old_main = old_sector[:2]
+                old_sub = old_sector[2:]
+                
+                if main[1] != old_main[1]:
+                    trans_table = path.maketrans('xy', 'yx')
+                    path = path.translate(trans_table)
+                
+                for current, old in (main, old_main), (sub, old_sub):
+                    if current[0] != old[0]:
+                        axis = current[1]
+                        path = path.replace(f'-{axis}', f'*{axis}')
+                        path = path.replace(f'+{axis}', f'-{axis}')
+                        path = path.replace(f'*{axis}', f'+{axis}')
+                
+                
+                # само выполнение уже измененного пути
+                commands = path.split(' ')
+                for command in commands:
+                    sign = command[0]
+                    offset = sign + '1'
+                    offset = int(offset)
+                    if command[1] == 'x':
+                        vposition = self.move(board, vposition, row=True, offset=offset)
+                    else:
+                        vposition = self.move(board, vposition, column=True, offset=offset)
+                
+                
+                # если сам промежуточный путь тоже должен бить при ходьбе
+                if smash:
+                    smash_list += [vposition]
+                # дальнейшее развитие пути и внесение дочерних путей в смэшлист
+                smash_list += generate_rotated_paths(vposition, path_data[1:], current_sector=current_sector)
+                # нужно для корректной модификации пути
+                old_sector = current_sector
+            
+            return smash_list
+        
+
+        for possible_move in generate_rotated_paths(position, self.path_data):
+            if board.grid[possible_move]['piece']:
+                yield possible_move
+                continue
+            else:
+                yield possible_move
